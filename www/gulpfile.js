65d1340dd37e98ebedbf89af89f7e9dde679c5b2
Alibn22 <a.baghchehban1@gmail.com>
2017-07-17 07:46:22 +0430

    adding gitignore

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -1,30 +1,35 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var uif = require('gulp-if');
-var cssnano = require('gulp-cssnano');
+var sd = require('gulp-lazy-minify');
 
-gulp.task('watch',['browserSync','useref'],function () {
-    gulp.watch('./css,js,html not minify/*.html',['useref']);
-    gulp.watch('./css,js,html not minify/*.html',browserSync.reload);
-    gulp.watch('./css,js,html not minify/js/*.js',browserSync.reload);
-    gulp.watch('./css,js,html not minify/css/*.css',browserSync.reload);
+gulp.task('watch',['browserSync'],function () {
+    gulp.watch('./src/*.html',browserSync.reload);
+    gulp.watch('./src/js/*.js',browserSync.reload);
+    gulp.watch('./src/css/*.css',browserSync.reload);
 });
 gulp.task('useref',function () {
-    return gulp.src('./css,js,html not minify/*.html')
+    return gulp.src('./src/*.html')
             .pipe(useref())
-            .pipe(uif('./css,js,html not minify/js/index.main.js',uglify()))
-            .pipe(uif('./css,js,html not minify/css/index.main.js',uglify()))
-            .pipe(gulp.dest('dist'))
-            .pipe(browserSync.reload({
-                stream:true
-            }))
+            .pipe(gulp.dest('./src/dist'))
+            //.pipe(browserSync.reload({
+             //   stream:true
+           // }))
 });
 gulp.task('browserSync',function () {
     browserSync.init({
         server:{
-            baseDir:'./css,js,html not minify'
+            baseDir:'./src'
         },
     })
+});
+gulp.task('minifyjs',function () {
+    return gulp.src(['./src/dist/js/app.main.js','./src/dist/js/vendor.main.js'])
+    .pipe(sd())
+    .pipe(gulp.dest('./src/dist/min/js'))
+});
+gulp.task('minifycss',function () {
+    return gulp.src(['./src/dist/css/app.main.css','./src/dist/css/vendor.main.css'])
+    .pipe(sd())
+    .pipe(gulp.dest('./src/dist/min/css'))
 });
